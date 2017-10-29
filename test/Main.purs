3fe06a05af20c3808bf391e30d22a9d2d5730346
Joe Kachmar <joseph.kachmar@gmail.com>
2017-10-29 16:10:07 -0400

    Adds some truly awful tests.

--- test/Main.purs
+++ test/Main.purs
@@ -13,9 +13,56 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console as EffLog
 import Control.Monad.Rec.Class (forever)
 import Control.Monad.Trans.Class (lift)
+import Data.Maybe (fromMaybe)
 import Data.Monoid (mempty)
 import Data.Newtype (unwrap, wrap)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
 
+--------------------------------------------------------------------------------
+main :: Eff _ Unit
+main = run [consoleReporter] do
+  describe "purescript-mqtt" do
+    describe "Roundtrip" do
+      it "should send and recover a message to/from the test broker" do
+        client <- liftEff $ connect url options
+
+        ref <- makeEmptyVar
+        _   <- forkAff $ runProcess $ mqttProcess client ref
+
+        liftEff $ subscribe client $ Topic "test/purescript-mqtt"
+        liftEff $ publish client (Topic "test/purescript-mqtt") (Message "test")
+        liftEff $ end client
+
+        msg <- takeVar ref
+        msg.topic   `shouldEqual` "test/purescript-mqtt"
+        msg.message `shouldEqual` "test"
+
+--------------------------------------------------------------------------------
+mqttConsumer
+  :: AVar {topic :: String, message :: String}
+  -> MQTTConsumer _ Unit
+mqttConsumer ref = forever do
+  e <- await
+  case e of
+    OnConnect -> pure unit
+    OnClose -> pure unit
+    OnMessage t m ->
+      lift $ putVar ({ topic : unwrap t, message : unwrap m}) ref
+
+--------------------------------------------------------------------------------
+mqttProcess
+  :: ∀ e
+   . Client
+  -> AVar {topic :: String, message :: String}
+  -> Process (Aff _) Unit
+mqttProcess client ref = do
+  let mqttProducer = mkMQTTProducer client
+  mqttProducer $$ mqttConsumer ref
+
+--------------------------------------------------------------------------------
 url :: BrokerUrl
 url = wrap "mqtt://broker.hivemq.com"
 
@@ -26,44 +73,3 @@ options =
   , username : mempty
   , password : mempty
   }
-
-mqttProcess :: ∀ e. Client -> Process (Aff _) Unit
-mqttProcess client =
-  let mqttProducer = mkMQTTProducer client
-  in mqttProducer $$ mqttConsumer
-
-main :: Eff _ _
-main = launchAff_ do
-  client <- liftEff $ connect url options
-  log $ "Connected to " <> unwrap url
-
-  let topic = Topic "test/purescript/mqtt"
-  liftEff $ subscribe client topic
-  log $ "Subscribed to " <> unwrap topic
-
-  _ <- forkAff $ runProcess $ mqttProcess client
-  log $ "Spawned coroutine process that logs events to stdout."
-
-  let msg = Message "test"
-  liftEff $ publish client topic msg
-  liftEff $ publish client topic msg
-  liftEff $ publish client topic msg
-  log $ "Published " <> unwrap msg <> " to " <> unwrap topic
-
-  delay $ Milliseconds 1000.0
-  liftEff $ end client
-  log $ "Disconnected from " <> unwrap url
-
-mqttConsumer :: ∀ e. MQTTConsumer _ Unit
-mqttConsumer = forever do
-  e <- await
-  case e of
-    OnConnect -> do
-      lift $ log "Connected to MQTT broker."
-    OnMessage t m -> do
-      lift
-        $ log
-        $ "Received message \"" <> unwrap m
-        <> "\" on topic \""     <> unwrap t <> "\""
-    OnClose -> do
-      lift $ log "Connection closed."
