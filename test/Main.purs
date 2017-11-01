22d390188b10f824391b5044ed952c384dcd2014
Joe Kachmar <joseph.kachmar@gmail.com>
2017-11-01 15:49:29 -0400

    Changes tests to cleanup properly and use a local MQTT broker

--- test/Main.purs
+++ test/Main.purs
@@ -1,45 +1,75 @@
 module Test.Main where
 
-import Prelude
 import Control.Monad.Aff
 import Control.Monad.Aff.AVar
 import Control.Monad.Aff.Console
 import Network.MQTT
 import Network.MQTT.Coroutine
+import Prelude
 
 import Control.Coroutine (Process, await, runProcess, ($$))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Rec.Class (forever)
 import Control.Monad.Trans.Class (lift)
+import Data.Either (isLeft)
+import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Data.Newtype (unwrap, wrap)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (run')
 
 --------------------------------------------------------------------------------
 main :: Eff _ Unit
-main = run [consoleReporter] do
-  describe "purescript-mqtt" do
-    describe "Roundtrip" do
-      it "should send and recover a message to/from the test broker" do
+main =
+  -- Set a longer timeout for testing against HiveMQ
+  run' { slow: 1000, timeout: Just 10000 } [consoleReporter] do
+  describe "purescript-mqttjs" do
+    describe "Connection" do
+      it "connects to and disconnects from the MQTT broker successfully" do
+        -- Connect to the MQTT broker
         client <- liftEff $ connect url options
 
+        -- Get an AVar and pass it off to the fiber running our MQTT process
         ref <- makeEmptyVar
-        _   <- forkAff $ runProcess $ mqttProcess client ref
+        fiber <- forkAff $ runProcess $ mqttProcess client ref
+
+        -- Disconnect from the MQTT broker
+        liftEff $ end client
+
+        -- Get the final value from the MQTT process and verify that no
+        -- exceptions were thrown.
+        res <- try $ joinFiber fiber
+        isLeft res `shouldEqual` false
+
+    describe "Roundtrip" do
+      it "roundtrips a message to and from the MQTT broker" do
+        -- Connect to the MQTT broker
+        client <- liftEff $ connect url options
+
+        -- Get an AVar and pass it off to the fiber running our MQTT process
+        ref <- makeEmptyVar
+        fiber <- forkAff $ runProcess $ mqttProcess client ref
 
         let topic   = Topic "test/purescript/mqtt"
         let message = Message "MQTT test message"
 
+        -- Subscribe to the test topic, publish a message, and close the client
         liftEff $ subscribe client topic
         liftEff $ publish client topic message
         liftEff $ end client
 
+        -- Verify that the topic and message received matches those sent
         msg <- takeVar ref
         msg.topic   `shouldEqual` unwrap topic
         msg.message `shouldEqual` unwrap message
+
+        -- Get the final value from the coroutine process and verify that
+        -- no exceptions were thrown.
+        res <- try $ joinFiber fiber
+        isLeft res `shouldEqual` false
 
 --------------------------------------------------------------------------------
 mqttConsumer
@@ -49,7 +79,7 @@ mqttConsumer ref = forever do
   e <- await
   case e of
     OnConnect -> pure unit
-    OnClose -> pure unit
+    OnClose   -> pure unit
     OnMessage t m ->
       lift $ putVar ({ topic : unwrap t, message : unwrap m}) ref
 
@@ -65,7 +95,7 @@ mqttProcess client ref = do
 
 --------------------------------------------------------------------------------
 url :: BrokerUrl
-url = wrap "mqtt://broker.hivemq.com"
+url = wrap "mqtt://localhost"
 
 options :: Options
 options =
